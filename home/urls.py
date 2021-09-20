c75b5aa912c11f1626cdb3e6759c71d543909296
kaushikashok45 <56879395+kaushikashok45@users.noreply.github.com>
2021-09-20 15:23:47 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('customers/',views.CustomerListView.as_view(),name='customers'),
-    path('customer/<uuid:pk>', views.CustomerDetailView.as_view(), name='customer-detail'),
+    path('customers/<uuid:pk>', views.CustomerDetailView.as_view(), name='customer-detail'),
     path('actionUrl/<uuid:cid>',views.transac,name='actionUrl'),
     path('payment/<uuid:tcid>/<uuid:fcid>',views.pay,name='payment'),
     path('process/<uuid:tcid>/<uuid:fcid>',views.process,name='process'),
